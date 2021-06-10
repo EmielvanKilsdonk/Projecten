951ffdc59226b59e44fb06cb6b1301200539b9a7
DeanMichiels <PS210022@edu.summacollege.nl>
2021-06-10 11:25:42 +0200

    Inlog en registratie
    Inlogpagina gemaakt en deel registratie

---
+++ Website/controle.php
@@ -0,0 +1,41 @@
+<?php
+    $connection = mysqli_connect("localhost","root","", "test01");
+
+    //INLOGGEN
+    if(isset($_GET['method']) && $_GET['method'] == 'login')
+    {
+      $wachtwoord = $_POST['inputPassword'];
+      $username = $_POST['inputUsername'];
+
+      $query = "SELECT * FROM persoonsgegevens WHERE gebruikersnaam = '$username' AND wachtwoord = '$wachtwoord'";
+      $resultaat = mysqli_query($connection, $query);
+
+
+      if (mysqli_num_rows($resultaat) > 0)
+      {
+        while($row = mysqli_fetch_array($resultaat, MYSQLI_NUM))
+        {
+          session_start();
+          $_SESSION['login'] = true;
+
+          header("location: index.php");
+          exit;
+        }
+      }
+      else {
+        header("location: index.php");
+        exit;
+      }
+    }
+    //REGISTREREN
+    else if(isset($_GET['method']) && $_GET['method'] == 'login')
+    {
+      
+    }
+    else
+    {
+      header("location: index.php");
+      exit;
+    }
+
+?>
--- Website/index.php
+++ Website/index.php
@@ -1,14 +1,22 @@
-<html lang='en'>
+<html lang='nl'>
 
 <head>
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel='stylesheet' href='master.css'>
+  <link rel='stylesheet' href='css/master.css'>
   <title>Summa College - Projecten</title>
 </head>
 
 <body>
+  <?php
+    session_start();
+    if (isset($_SESSION['login']) && $_SESSION['login'] === true)
+    {
+      echo $_SESSION['login'];
+      echo '<p><a class="btn btn-primary btn-lg" href="uitloggen.php" role="button">Uitloggen »</a></p>';
+    }
+  ?>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">
       <img src="img/logo.png" height="30" class="d-inline-block align-top" alt="">
@@ -28,7 +36,7 @@
           <a class="nav-link disabled" href="#">Indienen</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="login.html">Login</a>
+          <a class="nav-link" href="login.php">Login</a>
         </li>
       </ul>
       <span class="navbar-text">
@@ -42,8 +50,8 @@
     <div class="jumbotron">
       <div class="container">
         <h1 class="display-3">Projecten opleveren</h1>
-        <p>Op deze site is het mogelijk om je projecten voor het Summa College op te leveren. Deze kunnen dan door de opdrachtgever of deelnemers bekeken worden en beoordeerd.</p>
-        <p><a class="btn btn-primary btn-lg" href="login.html" role="button">Login »</a></p>
+        <p>Op deze site is het mogelijk om je projecten voor het Summa College op te leveren. Deze kunnen dan door u of de opdrachtgever bekeken worden en beoordeerd.</p>
+        <p><a class="btn btn-primary btn-lg" href="login.php" role="button">Login »</a></p>
       </div>
     </div>
   </main>
